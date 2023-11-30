a83650561626c6a53964a21256a7d5ab211b579a
Saskia Hiltemann <zazkia@gmail.com>
2023-11-30 12:47:13 +0100

    update test data

--- test-data/test-run.sh
+++ test-data/test-run.sh
@@ -9,11 +9,17 @@ NAME=$(basename $i .fa)
 
 echo "Running HMMsearch for $NAME\n"
 # run HMMEr search
-hmmsearch --domtblout $NAME.domtblout --cut_ga ../domains_v12.txt $i
+#hmmsearch --domtblout $NAME.domtblout --cut_ga ../domains_v12.txt $i
 
 echo "Running TAPscan Classify for $NAME\n"
 # run TAPscan script
-../tapscan_classify.pl $NAME.domtblout ../rules_v81.txt $NAME.output.1 $NAME.output.2 $NAME.output.3 ../coverage_values_v10.txt
+../tapscan_classify.pl \
+$NAME.domtblout \
+../rules_v81.txt \
+$NAME.output.1.family_classifications \
+$NAME.output.2.family_statistics \
+$NAME.output.3.subfamily_classifications \
+../coverage_values_v10.txt
 
 done
 
